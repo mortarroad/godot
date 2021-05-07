36defb2c83a3bfc21c75d73b61241700aa040a00
Morris Tabor <80684659+mortarroad@users.noreply.github.com>
2021-05-07 12:48:56 +0200

    Replace QuickHull with Bullet's convex hull computer. The code is based on the current version of thirdparty/vhacd and modified to use Godot's types.

---
+++ core/math/convex_hull.h
@@ -0,0 +1,135 @@
+/*************************************************************************/
+/*  convex_hull.h                                                        */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+/*
+Copyright (c) 2011 Ole Kniemeyer, MAXON, www.maxon.net
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
+subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
+
+#ifndef CONVEX_HULL_H
+#define CONVEX_HULL_H
+
+//#include "btAlignedObjectArray.h"
+//#include "btVector3.h"
+
+#include "core/local_vector.h"
+#include "core/vector.h"
+#include "core/math/geometry.h"
+#include "core/math/vector3.h"
+
+/// Convex hull implementation based on Preparata and Hong
+/// See http://code.google.com/p/bullet/issues/detail?id=275
+/// Ole Kniemeyer, MAXON Computer GmbH
+class ConvexHullComputer {
+private:
+    float compute(const void* coords, bool doubleCoords, int32_t stride, int32_t count, float shrink, float shrinkClamp);
+
+public:
+    class Edge {
+    private:
+        int32_t next;
+        int32_t reverse;
+        int32_t targetVertex;
+
+        friend class ConvexHullComputer;
+
+    public:
+        int32_t getSourceVertex() const
+        {
+            return (this + reverse)->targetVertex;
+        }
+
+        int32_t getTargetVertex() const
+        {
+            return targetVertex;
+        }
+
+        const Edge* getNextEdgeOfVertex() const // clockwise list of all edges of a vertex
+        {
+            return this + next;
+        }
+
+        const Edge* getNextEdgeOfFace() const // counter-clockwise list of all edges of a face
+        {
+            return (this + reverse)->getNextEdgeOfVertex();
+        }
+
+        const Edge* getReverseEdge() const
+        {
+            return this + reverse;
+        }
+    };
+
+    // Vertices of the output hull
+    Vector<Vector3> vertices;
+
+    // Edges of the output hull
+    LocalVector<Edge> edges;
+
+    // Faces of the convex hull. Each entry is an index into the "edges" array pointing to an edge of the face. Faces are planar n-gons
+    LocalVector<int32_t> faces;
+
+    /*
+		Compute convex hull of "count" vertices stored in "coords". "stride" is the difference in bytes
+		between the addresses of consecutive vertices. If "shrink" is positive, the convex hull is shrunken
+		by that amount (each face is moved by "shrink" length units towards the center along its normal).
+		If "shrinkClamp" is positive, "shrink" is clamped to not exceed "shrinkClamp * innerRadius", where "innerRadius"
+		is the minimum distance of a face to the center of the convex hull.
+
+		The returned value is the amount by which the hull has been shrunken. If it is negative, the amount was so large
+		that the resulting convex hull is empty.
+
+		The output convex hull can be found in the member variables "vertices", "edges", "faces".
+		*/
+    float compute(const float* coords, int32_t stride, int32_t count, float shrink, float shrinkClamp)
+    {
+        return compute(coords, false, stride, count, shrink, shrinkClamp);
+    }
+
+    // same as above, but double precision
+    float compute(const double* coords, int32_t stride, int32_t count, float shrink, float shrinkClamp)
+    {
+        return compute(coords, true, stride, count, shrink, shrinkClamp);
+    }
+
+	static Error convex_hull(const Vector<Vector3> &p_points, Geometry::MeshData &r_mesh);
+};
+
+
+#endif // CONVEX_HULL_H
